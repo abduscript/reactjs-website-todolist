57c464f82e2487f770d41ce4a853264b10b89b18
abduscript <abdupondokit@gmail.com>
2024-11-07 08:07:03 +0800

    react commit

---
+++ src/components/ToDoListButton.jsx
@@ -0,0 +1,33 @@
+
+function ToDoListButton(props) {
+    let id = props.id;
+    let currentIndex = props.tasks.findIndex((item)=>item.id == id)
+    let prevIndex = currentIndex - 1;
+    let nextIndex = currentIndex + 1;
+
+
+    let prevButton = '';
+    if(props.tasks[prevIndex] != undefined){
+        prevButton = '⬆️';
+    }else{
+        prevIndex = '';
+    }
+
+    let nextButton = '';
+    if(props.tasks[nextIndex] != undefined){
+        nextButton = '⬇️';
+    }else{
+        nextIndex = '';
+    }
+
+
+    return (
+        <>
+            <span><button onClick={() =>props.move(currentIndex, prevIndex)}>{prevButton}</button></span>
+            <span><button onClick={() =>props.move(currentIndex, nextIndex)}>{nextButton}</button></span>
+            <span><button onClick={() =>props.remove(props.id)}>🗑️</button></span>
+        </>
+    )
+}
+
+export default ToDoListButton
